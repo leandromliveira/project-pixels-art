54d2f003efebc9ecb849ace1931bc9f6a79bf79d
leandromliveira <leandrooliveira35@gmail.com>
2021-09-10 10:51:53 -0300

    subindo projeto para o github

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1>Projeto Pixel Art</h1>
+<p>Com o Pixel Art você pode despertar o artista que existe dentro de você.</p>
+<p>O botão VQV gera outra tela de pintura com a dimensão colocada no input, valores entre 5 e 25 pixels</p>
+<img src="/images/pixel-art.png" alt="pixel art">
+<h2> Avaliação do projeto: <h2>
+
+<img src="/images/nota-projeto.png" alt="avaliação do projeto" />
+<a href="https://github.com/tryber/sd-015-b-project-pixels-art/pull/10" target="_blank"></a>
---
+++ script.js
@@ -0,0 +1,88 @@
+const firstColor = document.getElementById('first-color');
+const secondColor = document.getElementById('second-color');
+const thirdColor = document.getElementById('third-color');
+const fourthColor = document.getElementById('fourth-color');
+const fifthColor = document.getElementById('fifth-color');
+const pixelBoard = document.getElementById('pixel-board');
+const allPixel = document.getElementsByClassName('pixel');
+const clearButton = document.getElementById('clear-board');
+const boardSize = document.getElementById('board-size');
+const generateBoardButton = document.getElementById('generate-board');
+const newColor = document.getElementById('new-color');
+
+function CreatePixelBoard(size) {
+  for (let index = 0; index < size; index += 1) {
+    const ul = document.createElement('div');
+    ul.Id = index + 1;
+    pixelBoard.appendChild(ul);
+    for (let index2 = 0; index2 < size; index2 += 1) {
+      const li = document.createElement('div');
+      li.className = 'pixel';
+      li.style.backgroundColor = 'white';
+      li.addEventListener('click', changeColor);
+      ul.appendChild(li);
+    }
+  }
+}
+CreatePixelBoard(5);
+
+function addSelectedClass(event) {
+  const checkSelected = document.querySelector('.selected');
+  checkSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+function changeColor(event) {
+  const pixel = event;
+  const ColorSelected = document.querySelector('.selected').style.backgroundColor;
+  pixel.target.style.backgroundColor = ColorSelected;
+}
+
+function clearColor() {
+  for (let index = 0; index < allPixel.length; index += 1) {
+    allPixel[index].style.backgroundColor = 'white';
+  }
+}
+
+function setPixelWidth() {
+  if (!boardSize.value) {
+    alert('Board inválido!');
+  } else if (boardSize.value < 5) {
+    pixelBoard.innerHTML = '';
+    CreatePixelBoard(5);
+  } else if (boardSize.value > 49) {
+    pixelBoard.innerHTML = '';
+    CreatePixelBoard(50);
+  } else {
+    pixelBoard.innerHTML = '';
+    CreatePixelBoard(boardSize.value);
+  }
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let index = 0; index < 6; index += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setPalletColors() {
+  firstColor.style.backgroundColor = 'black';
+  secondColor.style.backgroundColor = getRandomColor();
+  thirdColor.style.backgroundColor = getRandomColor();
+  fourthColor.style.backgroundColor = getRandomColor();
+  fifthColor.style.backgroundColor = 'white';
+}
+
+setPalletColors();
+
+newColor.addEventListener('click', setPalletColors);
+firstColor.addEventListener('click', addSelectedClass);
+secondColor.addEventListener('click', addSelectedClass);
+thirdColor.addEventListener('click', addSelectedClass);
+fourthColor.addEventListener('click', addSelectedClass);
+fifthColor.addEventListener('click', addSelectedClass);
+clearButton.addEventListener('click', clearColor);
+generateBoardButton.addEventListener('click', setPixelWidth);
